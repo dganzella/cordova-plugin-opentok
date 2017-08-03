c501d7ce28e50c9b98e9b766d5d179e14833ef65
Danilo Ganzella <dganzella@2mundos.net>
2017-08-03 18:49:00 -0300

    Install code correction

--- scripts/uncompactSDK.js
+++ scripts/uncompactSDK.js
@@ -1,6 +1,10 @@
 module.exports = function(context) {
     var IosSDKFile = "OpenTok-iOS-2.11.3.tar.bz2";
 
+    var exec = require('./exec/exec.js'),
+        Q = context.requireCordovaModule('q'),
+        deferral = new Q.defer();
+
     exec('tar -zxvf ./' + context.opts.plugin.dir + '/src/ios/' + IosSDKFile, function(err, out, code) {
         console.log('expanded');
     });
